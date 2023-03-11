b4ec98c1aa6521d4b74953c3b3e25b658a111e4f
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2023-03-11 09:26:33 -0300

    Add files via upload

---
+++ TesteCalculadora/src/main/java/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+
+    public double soma(double x, double y){
+        return  x + y;
+    }
+
+    public boolean verificaPar(double valorPar){
+        return  valorPar % 2 == 0;
+    }
+}
